4106604b00b5928833e567c4bb04841e21ab007c
D7-689 <louischan009@gmail.com>
2022-03-30 22:52:48 +0800

    commit

--- Latest-version/testing/settings.py
+++ Latest-version/testing/settings.py
@@ -122,7 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # The Add for heroku----------------------------#
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 #------------------------------------------------#
 
 STATIC_URL = '/static/'
